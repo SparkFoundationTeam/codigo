6d327cd395b1081caab3dd14f05cf19ce8de0301
Atharva Bhagat <atharva.r.bhagat@gmail.com>
2021-08-09 15:32:38 +0530

    UI v1.0

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import LandingPage from './landingPage/landingPage'
+function App() {
+    return (
+        <div className="App">
+            <LandingPage />
+            
+        </div>
+    );
+}
+
+export default App;
---
+++ src/landingPage/components/reasonCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './whyCodigo.css'
+
+
+const card = (props) => {
+    return (
+        <div className='reasonCard'>
+            
+            <div>
+                <img src={props.image}></img></div>
+            <div>
+                <h2>{props.title}</h2>
+                <h4>{props.line1}</h4>
+                <h4>{props.line2}</h4>
+            </div>
+            
+            
+        </div>
+
+    );
+}
+
+export default card;
