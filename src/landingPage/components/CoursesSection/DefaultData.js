5cc9e9e097b927bbe15cf4e750676e15675ef333
Bhavesh Mhadse <70565774+bhaveshmhadse@users.noreply.github.com>
2021-08-09 16:33:03 +0530

    Added Our Courses Section

---
+++ src/landingPage/components/CoursesSection/DefaultData.js
@@ -0,0 +1,30 @@
+import Logo from './Images/physics.png';
+import Bhavesh from './Images/bhavesh.jpg';
+
+export const CoursesArray = ['Baray', 'thike'];
+export const OurCoursesCardInfo = [
+  {
+    ImageLogo: Logo,
+    CourseName: 'HTML',
+    CourseInfo: 'sdfb  bhfdkhfb dkhbfkdlhbc  kfbhkhfdbfkdhb k bdfkbfdkdhfbs',
+    AuthorPic: Bhavesh,
+    AUthorName: 'Bhavesh Mhadse',
+    CourseDuration: '5 Hours',
+  },
+  {
+    ImageLogo: Logo,
+    CourseName: 'HTML',
+    CourseInfo: 'sdfb  bhfdkhfb dkhbfkdlhbc  kfbhkhfdbfkdhb k bdfkbfdkdhfbs',
+    AuthorPic: Bhavesh,
+    AUthorName: 'Bhavesh Mhadse',
+    CourseDuration: '5 Hours',
+  },
+  {
+    ImageLogo: Logo,
+    CourseName: 'HTML',
+    CourseInfo: 'sdfb  bhfdkhfb dkhbfkdlhbc  kfbhkhfdbfkdhb k bdfkbfdkdhfbs',
+    AuthorPic: Bhavesh,
+    AUthorName: 'Bhavesh Mhadse',
+    CourseDuration: '5 Hours',
+  },
+];
