8fe5317f471cf84da2a96ac5e6d05557003efb43
bhaveshmhadse <bhaveshmhadse9@gmail.com>
2021-08-17 22:12:28 +0530

    Added Bhavesh's Insta n Linked In Url

--- src/landingPage/components/CoursesSection/DefaultData.js
+++ src/landingPage/components/CoursesSection/DefaultData.js
@@ -1,42 +1,41 @@
 import Bhavesh from '../../../resources/bhavesh.jpg';
 import JSBhavesh from '../../../resources/JSBhavesh.png';
-import HtmlAtharva from '../../../resources/HTMLAtharva.png'
-import JavaAdika from '../../../resources/JAVAAdika.png'
-import PythonVaishnavi from '../../../resources/PYTHONVaishnavi.png'
+import HtmlAtharva from '../../../resources/HTMLAtharva.png';
+import JavaAdika from '../../../resources/JAVAAdika.png';
+import PythonVaishnavi from '../../../resources/PYTHONVaishnavi.png';
 
 export const CoursesArray = ['Web Development', 'Python Development', 'JavaScript Series', 'Java Development'];
 export const OurCoursesCardInfo = [
-    {
-        ImageLogo: HtmlAtharva,
-        CourseName: 'HTML',
-        CourseInfo: 'The complete HTML Course to kick-start Web Development',
-        AuthorPic: Bhavesh,
-        AUthorName: 'Atharva Bhagat',
-        CourseDuration: '1 Hour',
-    },
-    {
-        ImageLogo: JSBhavesh,
-        CourseName: 'JavaScript',
-        CourseInfo: 'Learn Javascript Concepts and start building Applications',
-        AuthorPic: Bhavesh,
-        AUthorName: 'Bhavesh Mhadse',
-        CourseDuration: '2 Hours',
-    },
-    {
-        ImageLogo: JavaAdika,
-        CourseName: 'Java',
-        CourseInfo: 'Java Programming explained thorougly to enter beautiful world of Java',
-        AuthorPic: Bhavesh,
-        AUthorName: 'Adika Karnataki',
-        CourseDuration: '2 Hours',
-    },
-    {
-        ImageLogo: PythonVaishnavi,
-        CourseName: 'Python',
-        CourseInfo: 'Complete Python Course loaded with various interesting Concepts',
-        AuthorPic: Bhavesh,
-        AUthorName: 'Vaishnavi Korgaonkar',
-        CourseDuration: '2 Hours',
-    },
-
+  {
+    ImageLogo: HtmlAtharva,
+    CourseName: 'HTML',
+    CourseInfo: 'The complete HTML Course to kick-start Web Development',
+    AuthorPic: Bhavesh,
+    AUthorName: 'Atharva Bhagat',
+    CourseDuration: '1 Hour',
+  },
+  {
+    ImageLogo: JSBhavesh,
+    CourseName: 'JavaScript',
+    CourseInfo: 'Learn Javascript Concepts and start building Applications',
+    AuthorPic: Bhavesh,
+    AUthorName: 'Bhavesh Mhadse',
+    CourseDuration: '2 Hours',
+  },
+  {
+    ImageLogo: JavaAdika,
+    CourseName: 'Java',
+    CourseInfo: 'Java Programming explained thorougly to enter beautiful world of Java',
+    AuthorPic: Bhavesh,
+    AUthorName: 'Adika Karnataki',
+    CourseDuration: '2 Hours',
+  },
+  {
+    ImageLogo: PythonVaishnavi,
+    CourseName: 'Python',
+    CourseInfo: 'Complete Python Course loaded with various interesting Concepts',
+    AuthorPic: Bhavesh,
+    AUthorName: 'Vaishnavi Korgaonkar',
+    CourseDuration: '2 Hours',
+  },
 ];
--- src/landingPage/components/CoursesSection/OurCourses.js
+++ src/landingPage/components/CoursesSection/OurCourses.js
@@ -1,69 +1,63 @@
 import React, { useState } from 'react';
 
 import {
-    OurCoursesSection,
-    SectionHeader,
-    Timing,
-    CourseSelectorButtonContainer,
-    CourseSelectorButton,
-    CoursesCardContainer,
-    CoursesCard,
-    CourseCardHeader,
-    CourseCardInfo,
-    ButtonContainerDiv,
-    TutorContainer,
-    TutorImage,
-    TutorName,
-    CourseIcon,
-    TutorImageContainer,
-    AllCourses
+  OurCoursesSection,
+  SectionHeader,
+  Timing,
+  CourseSelectorButtonContainer,
+  CourseSelectorButton,
+  CoursesCardContainer,
+  CoursesCard,
+  CourseCardHeader,
+  CourseCardInfo,
+  ButtonContainerDiv,
+  TutorContainer,
+  TutorImage,
+  TutorName,
+  CourseIcon,
+  TutorImageContainer,
+  AllCourses,
 } from '../../../global.styles';
 
-import randomizer from '../../../randomizer'
+import randomizer from '../../../randomizer';
 
-import {
-    CoursesArray ,
-    OurCoursesCardInfo,
-    
-
-} from './DefaultData';
+import { CoursesArray, OurCoursesCardInfo } from './DefaultData';
 
 const OurCourses = () => {
-    const [coursesArray, setcoursesArray] = useState(CoursesArray);
-    const [CardInfoArray, setCardInfoArray] = useState(randomizer(OurCoursesCardInfo));
-    
+  const [coursesArray, setcoursesArray] = useState(CoursesArray);
+  const [CardInfoArray, setCardInfoArray] = useState(randomizer(OurCoursesCardInfo));
 
-    return (
-        <OurCoursesSection>
-            <SectionHeader>Featured Courses</SectionHeader>
-            <ButtonContainerDiv>
-                <CourseSelectorButtonContainer>
-                    {coursesArray.map(eachCourse => (
-                        <CourseSelectorButton>{eachCourse}</CourseSelectorButton>
-                    ))}
-                </CourseSelectorButtonContainer>
-            </ButtonContainerDiv>
-            <CoursesCardContainer>
-                {CardInfoArray.map(eachObj => {
-                    return (
-                        <CoursesCard>
-                            <CourseIcon src={eachObj.ImageLogo} />
-                            <CourseCardHeader>{eachObj.CourseName}</CourseCardHeader>
-                            <CourseCardInfo>{eachObj.CourseInfo}</CourseCardInfo>
-                            <TutorContainer>
-                                <TutorImageContainer>
-                                    <TutorImage src={eachObj.AuthorPic} />
-                                </TutorImageContainer>
-                                <TutorName>{eachObj.AUthorName}</TutorName>
-                                <Timing>{eachObj.CourseDuration}</Timing>
-                            </TutorContainer>
-                        </CoursesCard>
-                    );
-                })}
-            </CoursesCardContainer>
-            <AllCourses>View All Courses</AllCourses>
-        </OurCoursesSection>
-    );
+  return (
+    <OurCoursesSection>
+      <SectionHeader>Featured Courses</SectionHeader>
+      <ButtonContainerDiv>
+        <CourseSelectorButtonContainer>
+          {coursesArray.map(eachCourse => (
+            <CourseSelectorButton>{eachCourse}</CourseSelectorButton>
+          ))}
+        </CourseSelectorButtonContainer>
+      </ButtonContainerDiv>
+      <CoursesCardContainer>
+        {CardInfoArray.map(eachObj => {
+          return (
+            <CoursesCard>
+              <CourseIcon src={eachObj.ImageLogo} />
+              <CourseCardHeader>{eachObj.CourseName}</CourseCardHeader>
+              <CourseCardInfo>{eachObj.CourseInfo}</CourseCardInfo>
+              <TutorContainer>
+                <TutorImageContainer>
+                  <TutorImage src={eachObj.AuthorPic} />
+                </TutorImageContainer>
+                <TutorName>{eachObj.AUthorName}</TutorName>
+                <Timing>{eachObj.CourseDuration}</Timing>
+              </TutorContainer>
+            </CoursesCard>
+          );
+        })}
+      </CoursesCardContainer>
+      <AllCourses>View All Courses</AllCourses>
+    </OurCoursesSection>
+  );
 };
 
 export default OurCourses;
