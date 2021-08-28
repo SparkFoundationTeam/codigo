411c27d13ff9f815d2713f08c6c8c6020398ec43
Bhavesh Mhadse <70565774+bhaveshmhadse@users.noreply.github.com>
2021-08-28 13:55:02 +0530

    Created Quiz Page/Component with all the functionalities, also added styles.

---
+++ src/Quiz/DefaultQuizQuestions.js
@@ -0,0 +1,27 @@
+export const QuizArr = [
+  {
+    question: 'What comes after 1',
+    options: ['2', '3', '4', '5'],
+    answer: 0, // <- Index of answer array
+  },
+  {
+    question: 'Who is Best In Javascript?',
+    options: ['Vaishnavi', 'Adika', 'Bhavesh', 'Atharva'],
+    answer: 2, // <- Index of answer array
+  },
+  {
+    question: 'Who is Best In CSS?',
+    options: ['Vaishnavi', 'Atharva', 'Bhavesh', 'Adika'],
+    answer: 1, // <- Index of answer array
+  },
+  {
+    question: 'Who is Best In Typescript?',
+    options: ['Bhavesh', 'Vaishnavi', 'Adika', 'Atharva'],
+    answer: 0, // <- Index of answer array
+  },
+  {
+    question: 'Keyword used in Python to declare a function?',
+    options: ['function', 'def', 'func', 'Def'],
+    answer: 1, // <- Index of answer array
+  },
+];
